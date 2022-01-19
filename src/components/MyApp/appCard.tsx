c2893fa3b65c26170f42169f488fcdb29f01d533
yudate <y0303noki@gmail.com>
2022-01-20 00:28:04 +0900

    色々やった

--- src/components/MyApp/appCard.tsx
+++ src/components/MyApp/appCard.tsx
@@ -1,36 +1,50 @@
 //  AppCardのコンポーネント
 import Image from 'next/image';
 
-const AppCard = ({ appName, appDescription }: { appName: string; appDescription: string }) => {
+const AppCard = ({
+  appName,
+  appDescription,
+  tags,
+  githubUrl,
+}: {
+  appName: string;
+  appDescription: string;
+  tags: string[];
+  githubUrl: string;
+}) => {
   return (
     <div className='flex flex-col h-full m-4 min-w-max w-96 max-w-md'>
-      <div className='max-w-sm rounded overflow-hidden shadow-lg '>
+      <div className='max-w-sm shadow-lg'>
         <div className='flex flex-row pt-4'>
-          <img
-            height={80}
-            width={80}
-            className='rounded-2xl ml-4 h-24 w-24'
-            src='/images/dogsample.jpg'
-          />
-          {/* <Image
-            src='/images/dogsample.jpg'
-            alt='My avatar'
-            className='rounded-2xl py-4 ml-4 h-24 w-24 object-cover'
-          /> */}
-          {/* </div> */}
+          <div className='ml-4 h-24 w-24  min-h-24 min-w-24'>
+            <Image
+              height={80}
+              width={80}
+              src='/images/dogsample.jpg'
+              alt='My avatar'
+              className='rounded-2xl'
+            />
+          </div>
           <div className='ml-4'>
             <div className='font-bold text-xl mb-2'>{appName}</div>
             <p className='text-gray-700 text-base'>{appDescription}</p>
           </div>
         </div>
+        {/* githubのurl */}
+        <div className='m-4'>
+          <a href={githubUrl}>{githubUrl}</a>
+        </div>
+
         {/* タグ */}
         <div className='px-6 pt-4 pb-2'>
-          <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
-            #flutter
-          </span>
-          <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
-            #firebase
-          </span>
+          {tags.map((tag) => (
+            <span
+              key={tag}
+              className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'
+            >
+              #{tag}
+            </span>
+          ))}
         </div>
       </div>
     </div>
--- src/components/MyApp/myApp.tsx
+++ src/components/MyApp/myApp.tsx
@@ -4,14 +4,40 @@ import AppCard from './appCard';
 const MyApp = () => {
   return (
     <>
-      <div className='flex flex-row overflow-scroll'>
-        <AppCard appName={'アプリA'} appDescription={'アプリAの説明です。説明説明！！！'}></AppCard>
-        <AppCard
-          appName={'アプリB'}
-          appDescription={'アプリbの説明です。説明説明アプリBですよ〜！！！！あはは'}
-        ></AppCard>
-        <AppCard appName={'アプリC'} appDescription={'アプリCの説明'}></AppCard>
-      </div>
+      <ul className='flex overflow-scroll'>
+        <li>
+          <AppCard
+            appName={'個人サイト'}
+            appDescription={'このサイト'}
+            tags={['Next.js', 'tailwind css']}
+            githubUrl='https://nextjs.org/learn/basics/assets-metadata-css/global-styles'
+          ></AppCard>
+        </li>
+        <li>
+          <AppCard
+            appName={'アプリA'}
+            appDescription={'アプリAの説明です。説明説明！！！'}
+            tags={['flutter', 'firebase']}
+            githubUrl='https://nextjs.org/learn/basics/assets-metadata-css/global-styles'
+          ></AppCard>
+        </li>
+        <li>
+          <AppCard
+            appName={'アプリB'}
+            appDescription={'アプリbの説明です。説明説明アプリBですよ〜！！！！あはは'}
+            tags={['ios']}
+            githubUrl='test'
+          ></AppCard>
+        </li>
+        <li>
+          <AppCard
+            appName={'アプリC'}
+            appDescription={'アプリCの説明'}
+            tags={['android']}
+            githubUrl='test'
+          ></AppCard>
+        </li>
+      </ul>
     </>
   );
 };
