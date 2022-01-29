1867262af7c487d5f43f7ec5a41474ad4e8dfa21
yudate <y0303noki@gmail.com>
2022-01-29 16:54:24 +0900

    Siではなくsiでした

--- src/components/SkilSet/skillSet.tsx
+++ src/components/SkilSet/skillSet.tsx
@@ -7,18 +7,17 @@ import {
   SiNextdotjs,
   SiTypescript,
 } from 'react-icons/si';
-import { FaGithub } from 'react-icons/fa';
 
 const SkillSet = () => {
   return (
     <>
       <div className='grid grid-cols-4 gap-4 sm:flex'>
-        {/* <SiIonic size={50}></SiIonic>
+        <SiIonic size={50}></SiIonic>
         <SiTypescript size={50}></SiTypescript>
         <SiAngular size={50}></SiAngular>
         <SiFirebase size={50}></SiFirebase>
         <SiNextdotjs size={50}> </SiNextdotjs>
-        <SiFlutter size={50}></SiFlutter> */}
+        <SiFlutter size={50}></SiFlutter>
       </div>
     </>
   );
