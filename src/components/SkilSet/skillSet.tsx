6858f1f1ef163694a37ddfcd29abafedd3ffd633
yudate <y0303noki@gmail.com>
2022-01-29 16:47:44 +0900

    コメントアウト

--- src/components/SkilSet/skillSet.tsx
+++ src/components/SkilSet/skillSet.tsx
@@ -12,12 +12,12 @@ const SkillSet = () => {
   return (
     <>
       <div className='grid grid-cols-4 gap-4 sm:flex'>
-        <SiIonic size={50}></SiIonic>
+        {/* <SiIonic size={50}></SiIonic>
         <SiTypescript size={50}></SiTypescript>
         <SiAngular size={50}></SiAngular>
         <SiFirebase size={50}></SiFirebase>
         <SiNextdotjs size={50}> </SiNextdotjs>
-        <SiFlutter size={50}></SiFlutter>
+        <SiFlutter size={50}></SiFlutter> */}
       </div>
     </>
   );
