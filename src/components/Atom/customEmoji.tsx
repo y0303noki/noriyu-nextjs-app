29d2a5879cf286c7f11de3c1e45e2bba2ca855c7
yudate <y0303noki@gmail.com>
2022-01-30 23:09:58 +0900

    mytopsong

--- src/components/Atom/customEmoji.tsx
+++ src/components/Atom/customEmoji.tsx
@@ -3,6 +3,7 @@ import { Emoji } from 'emoji-mart';
 const DIARY = 'diary';
 const DEVELOP = 'develop';
 const GAME = 'game';
+const MUSIC = 'music';
 
 const CustomEmoji = ({ icon = '' }: { icon?: string }) => {
   let emoji = '';
@@ -15,6 +16,9 @@ const CustomEmoji = ({ icon = '' }: { icon?: string }) => {
       break;
     case GAME:
       emoji = 'video_game';
+      break;
+    case MUSIC:
+      emoji = 'headphones';
       break;
     default:
       emoji = 'smile';
