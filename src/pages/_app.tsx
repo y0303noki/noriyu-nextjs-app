a321affd075300e3ec95f3e028a8efdb482b18cb
yudate <y0303noki@gmail.com>
2022-01-31 22:46:36 +0900

    gタグ実装

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,24 @@ import 'tailwindcss/tailwind.css';
 import '../../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { GA_TRACKING_ID, pageview } from '../../libs/gtag';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    // GA_TRACKING_ID が設定されていない場合は、処理終了
+    if (!GA_TRACKING_ID) return;
+
+    const handleRouteChange = (url: string) => {
+      pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <ThemeProvider attribute='class'>
       <Component {...pageProps} />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 import Seo from '../components/seo';
+import { GA_TRACKING_ID } from '../../libs/gtag';
 
 const MyDocument = () => {
   const baseUrl = process.env.NEXT_PUBLIC_HOST;
@@ -15,6 +16,23 @@ const MyDocument = () => {
           pageImgWidth={1280}
           pageImgHeight={960}
         ></Seo>
+        {GA_TRACKING_ID && (
+          <>
+            <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+        `,
+              }}
+            />
+          </>
+        )}
       </Head>
       <body className='dark:bg-gray-800'>
         <Main />
