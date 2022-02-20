327249411bb8d33d204cea9fd5dfbfbacf7010cd
yudate <y0303noki@gmail.com>
2022-02-20 13:28:46 +0900

    pwa対応

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
+// // /next.config.js
+// /** @type {import('next/dist/next-server/server/config-shared').NextConfig} */
+
+// const withPWA = require('next-pwa');
+
+// module.exports = withPWA({
+//   pwa: {
+//     dest: 'public', // swの出力ディレクトリ
+//     // runtimeCaching: []
+//   },
+// });
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
