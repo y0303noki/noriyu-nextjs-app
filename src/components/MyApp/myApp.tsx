a011275f435b302dbb4d601ed4eec036b9eeafad
yudate <y0303noki@gmail.com>
2022-03-25 16:06:48 +0900

    myapp追加

--- src/components/MyApp/myApp.tsx
+++ src/components/MyApp/myApp.tsx
@@ -12,11 +12,24 @@ const MyApp = () => {
     appStoreUrl: '',
     zasetuReason: '',
   };
+  const appInfo2: appInfo = {
+    name: 'プレイリスト作成アプリ',
+    appDescription: 'SpotifyのAPIを使ってプレイリストを作成します',
+    iconPath: '/images/logo/Spotify_Icon_Green_Coated.png',
+    tags: ['Next.js', 'tailwind', 'SpotifyAPI'],
+    githubUrl: '',
+    webUrl: '',
+    appStoreUrl: '',
+    zasetuReason: '',
+  };
   return (
     <>
       <ul className='grid grid-cols-2 sm:grid-cols-3'>
         <li>
           <AppCard appCard={appInfo}></AppCard>
+        </li>
+        <li>
+          <AppCard appCard={appInfo2}></AppCard>
         </li>
       </ul>
     </>
